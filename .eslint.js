613f9d5a1bcbe0704d1e2b9dbd03e1f1b937e779
Viktoryia Bondarava <Viktoryia.Bondarava@itechart-group.com>
2019-05-22 09:52:31 +0300

    added eslint with prettier

---
+++ .eslint.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        ecmaVersion: 2017, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+    },
+};
