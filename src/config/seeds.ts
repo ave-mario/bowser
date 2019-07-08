3abada7a3038f71ef4ceb79dfd381f1886725c31
Furmanov19 <artem.furmanov2014@mail.ru>
2019-07-08 12:43:14 +0300

    feat: add env verification
    add env test verification
    for seeds

--- src/config/seeds.ts
+++ src/config/seeds.ts
@@ -1,18 +1,20 @@
 import { Employee } from '../models/employees.model';
 import employeeSeeds from '../seeds/employee.seeds';
+import { config } from '../config/environment';
 
 export default async function(): Promise<Error | void> {
   const employeeCount = await Employee.countDocuments();
-
-  if (employeeCount === 0)
-    return Employee.remove(
-      {},
-      (): void => {
-        for (let seed of employeeSeeds) {
-          let newEmployee = new Employee(seed);
-          newEmployee.save();
+  if (config.app.environment !== 'test') {
+    if (employeeCount === 0)
+      return Employee.remove(
+        {},
+        async (): Promise<void> => {
+          for (let seed of employeeSeeds) {
+            let newEmployee = new Employee(seed);
+            await newEmployee.save();
+          }
+          console.log('Employee seeded');
         }
-        console.log('Employee seeded');
-      }
-    ).catch(err => console.log(err));
+      ).catch(err => console.log(err));
+  }
 }
