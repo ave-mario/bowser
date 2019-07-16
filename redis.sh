ba75ddbb66ea546df59e7626c1d4182f1cffdbd9
Furmanov19 <artem.furmanov2014@mail.ru>
2019-07-16 10:30:50 +0300

    feat: add bash script
    add bash script for docker start and
    remove useless react-scripts. change
    mongo version in circleci config

--- redis.sh
+++ redis.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
+exec 2>redis.sh.log.txt
 app="api-redis"
-redis =  docker ps  -f name="$app"
-if  [ -z "$redis"]; then
-  docker run -p 6379:6379 --name "$app" -d redis:5 
- else if docker ps -a -f "name=$app"; then
-   docker restart "$app"
-  fi
-fi
+  docker stop $app
+  docker rm $app 
+  docker run -p 6379:6379 --name $app -d redis:5 
